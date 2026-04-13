72437c03c25784f73c20a495a2600f2ade712f74
eslehmberg <66133773+eslehmberg@users.noreply.github.com>
2026-04-13 09:58:46 -0400

    Update Github+R_tutorial_Mar2026.R

--- Github+R_tutorial_Mar2026.R
+++ Github+R_tutorial_Mar2026.R
@@ -7,7 +7,7 @@ install.packages("usethis") # used to link git and R (more info: https://github.
 library(usethis)
 
 #### generate a token to link github and activate it ####
-# this code will open a page in your browser to generate a token for linking git and R (we will hop to git for this)
+# this code will open a page in your browser to generate a token for linking git and R (we will hop to git for this and talk security, time length, blah blah)
 usethis::create_github_token()
 # this will generate a personal access token (PAT) that you need to use...
 
@@ -18,4 +18,8 @@ gitcreds::gitcreds_set()
 
 #### OFF R: clone the LEARNING github to your local computer (https://github.com/eslehmberg/LEARNING) ####
 
-#### open the 
+# there are several ways to do this, my preferred method is just using the gui but you can do it through the shell (terminal if your on a mac)
+# instructions for shell cloning can be found here: https://happygitwithr.com/push-pull-github
+
+### cloning to R
+# start a new project: File > New Project > Version Control > Git
