705a321cae8216f85f5d3a8f2a64d99e37c89c83
eslehmberg <66133773+eslehmberg@users.noreply.github.com>
2026-04-13 09:53:05 -0400

    Update Github+R_tutorial_Mar2026.R

--- Github+R_tutorial_Mar2026.R
+++ Github+R_tutorial_Mar2026.R
@@ -7,12 +7,15 @@ install.packages("usethis") # used to link git and R (more info: https://github.
 library(usethis)
 
 #### generate a token to link github and activate it ####
-# this code will open a page in your browser to generate a token for linking git and R
+# this code will open a page in your browser to generate a token for linking git and R (we will hop to git for this)
 usethis::create_github_token()
 # this will generate a personal access token (PAT) that you need to use...
 
 # ...here
 gitcreds::gitcreds_set()
+# this will prompt a password or PAT request in the console
+# copy / paste your token to the console
 
+#### OFF R: clone the LEARNING github to your local computer (https://github.com/eslehmberg/LEARNING) ####
 
 #### open the 
