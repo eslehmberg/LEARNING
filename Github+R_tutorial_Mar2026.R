31ec9d477e8ae8afc8deeca76dff0be798dfd3ec
eslehmberg <66133773+eslehmberg@users.noreply.github.com>
2026-04-13 10:19:07 -0400

    Update Github+R_tutorial_Mar2026.R

--- Github+R_tutorial_Mar2026.R
+++ Github+R_tutorial_Mar2026.R
@@ -1,5 +1,5 @@
 ### git with R
-### for much more in depth info/troubleshooting, go here: https://happygitwithr.com/
+### for much more in depth info/troubleshooting for github + R, go here: https://happygitwithr.com/
 
 
 #### load packages ####
@@ -21,6 +21,17 @@ gitcreds::gitcreds_set()
 # there are several ways to do this, my preferred method is just using the gui but you can do it through the shell (terminal if your on a mac)
 # instructions for shell cloning can be found here: https://happygitwithr.com/push-pull-github
 
-### cloning to R
+#### ON R: the LEARNING github to your local computer (https://github.com/eslehmberg/LEARNING) ####
 # start a new project: File > New Project > Version Control > Git
 # this will open a window that asks for the URL of your repository ("repo"), whether you'd like to use version control (yes) and where you'd like to store it
+
+
+#### using Mike's provided script ####
+# 1. open it in your project
+# 2. write your name or a message right after the main header
+# 3. push that change
+# 4. pull all the other changes (you may have to do this more than once, depending on how fast people type)
+# 5. look at the history of changes (version control)
+#### general tips for version control ####
+# sometimes a good idea to check if changes have been made before you begin working on something
+# **ALWAYS** push changes before you stop working on something --> this ensures your collaborators aren't overwriting or doing the same work as you while you're in meatspace living your life
